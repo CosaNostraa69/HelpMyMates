45c0c0bf340a6d87f168c2694169bd8b8e02436a
CosaNostraa69 <lare.axelange.simplon@gmail.com>
2024-04-19 11:37:42 +0200

    Add Font Awesome CSS and update layout and navbar styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import NavbarWrapper from "./components/NavbarWrapper";
 import Footer from "./components/Footer";
 import { ThemeProvider } from "next-themes";
 
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
 export const metadata = {
   title: "Help my Mates",
   description: "Connect with your friends and family",
@@ -19,9 +21,7 @@ export default async function RootLayout({
       <body>
         <ThemeProvider attribute="class">
           <NavbarWrapper />
-          <div>
-            <main>{children}</main>
-          </div>
+          <main style={{ paddingBottom: "6rem" }}>{children}</main>
           <Footer />
         </ThemeProvider>
       </body>
